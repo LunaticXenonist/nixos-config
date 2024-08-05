f437f72debe999f7355988220b8d6e06d37ac1b5
Cole Gibbs <colemgibbs@gmail.com>
2024-08-05 16:08:06 -0400

    flake.nix rewrite for refactor/upgrade

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "LunaticXenonist's Flake for NixOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -16,19 +16,18 @@
 
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-    	specialArgs = { inherit inputs; };
-    	system = "x86_64-linux";
-	modules = [
-		./configuration.nix
-		home-manager.nixosModules.home-manager
-		{
-		  home-manager.useGlobalPkgs = true;
-		  home-manager.useUserPackages = true;
-		  home-manager.users.cole = import ./home.nix;
-		}
-	];
-    };
+  outputs = {...}@inputs: {
+	let 
+   		lib = inputs.nixpkgs.lib;
+		user = "cole";
+		sysPkgs = input.nixpkgs {
+			inherit system;
+			config.allowUnfree = true;
+		};
+  	in { 
+		nixosConfigurations = import ./hosts/default.nix {
+			inherit lib, user, inputs, sysPkgs;
+    		};
+  	};
   };
 }

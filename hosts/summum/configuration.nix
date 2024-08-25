8561bd2f9eb614e3075840f617f36f2f159b9d90
Cole Gibbs <colemgibbs@gmail.com>
2024-08-25 19:26:18 -0400

    added drive and fstrim

--- hosts/summum/configuration.nix
+++ hosts/summum/configuration.nix
@@ -7,4 +7,15 @@
 		enable = true;
 		enable32Bit = true;
 	};
+
+	services.fstrim.enable = true;
+
+	systemd.tmpfiles.rules = ["d /home/cole/hdd 755 ${user} users"];
+
+	
+
+	fileSystems."/home/cole/hdd" = {
+		device = "/dev/disk/by-uuid/910737de-240f-4d98-8931-a292a4e70f78";
+		fsType = "ext4";
+		};
 }

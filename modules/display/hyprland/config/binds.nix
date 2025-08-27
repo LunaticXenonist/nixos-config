7e1582e1af763b3a42f3704e3946e301cf82f213
Cole Gibbs <colemgibbs@gmail.com>
2025-08-27 14:51:00 -0400

    binds file

---
+++ modules/display/hyprland/config/binds.nix
@@ -0,0 +1,36 @@
+{pkgs, host, lib, ...}: {
+    wayland.windowManager.hyprland.settings = {
+      bind = [
+        "SUPER, r, exec, wofi --show=run"
+		    "SUPER, q, exec, kitty"
+		    "SUPER, c, killactive"
+		    "SUPER, m, exit"
+		    "SUPER, f, fullscreen"
+		    "SUPER, 1, workspace, 1"
+		    "SUPER, 2, workspace, 2"
+		    "SUPER, 3, workspace, 3"
+		    "SUPER, 4, workspace, 4"
+		    "SUPER, 5, workspace, 5"
+		    "SUPER, 6, workspace, 6"
+		    "SUPER, 7, workspace, 7"
+		    "SUPER, 8, workspace, 8"
+		    "SUPER, 9, workspace, 9"
+		    "SUPER, 0, workspace, 10"
+		    "SUPER SHIFT, 1, movetoworkspace, 1"
+		    "SUPER SHIFT, 2, movetoworkspace, 2"
+		    "SUPER SHIFT, 3, movetoworkspace, 3"
+		    "SUPER SHIFT, 4, movetoworkspace, 4"
+		    "SUPER SHIFT, 5, movetoworkspace, 5"
+		    "SUPER SHIFT, 6, movetoworkspace, 6"
+		    "SUPER SHIFT, 7, movetoworkspace, 7"
+		    "SUPER SHIFT, 8, movetoworkspace, 8"
+		    "SUPER SHIFT, 9, movetoworkspace, 9"
+		    "SUPER SHIFT, 0, movetoworkspace, 10"
+		    "SUPER, mouse_down, workspace, e+1"
+		    "SUPER, mouse_up, workspace, e-1"
+		    "ALT, space, exec, makoctl dismiss -a"
+        "SUPER SHIFT ALT, p, exec, hyprshot -m window -m active --clipboard-only"
+        "SUPER SHIFT, p, exec, hyprland -m window -m active -o ~/Pictures/Screenshots"
+      ];
+    };
+  }
